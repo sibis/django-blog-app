ff3b82293bbbc91df83ecb1a6ec9e7e71350b783
Sibi <sibi.kabil@gmail.com>
2019-11-19 04:15:23 +0530

    make it better

--- modistabox_api/settings.py
+++ modistabox_api/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'modistabox_api.urls'
@@ -82,6 +83,8 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     # uncomment for production to disallow browsable APIs
