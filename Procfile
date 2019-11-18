8e56f4cce202248b7c2a56c6aa23b585353caa4d
Sibi <sibi.kabil@gmail.com>
2019-11-19 04:04:02 +0530

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn modistabox_api.wsgi --log-file -
--- modistabox_api/settings.py
+++ modistabox_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -101,13 +102,16 @@ WSGI_APPLICATION = 'modistabox_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=0)
+DATABASES['default']['ENGINE'] = 'psqlextra.backend'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
